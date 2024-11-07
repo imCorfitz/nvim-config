90ef169aa4d3cb96fb3ea21882fd3091788deb82
Corfitz <hi@imcorfitz.com>
2024-11-07 19:02:40 +0100

    updating nvim theme

--- lua/corfitz/plugins/colorscheme.lua
+++ lua/corfitz/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   {
     "navarasu/onedark.nvim",
+    priority = 1000,
     config = function()
       require("onedark").setup({
         style = "deep", -- Choose 'dark', 'darker', 'cool', 'warm', or 'deep'
