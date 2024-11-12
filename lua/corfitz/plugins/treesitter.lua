194b9eecec64c0ce96eb2d5642a7c6496552992a
Corfitz <hi@imcorfitz.com>
2024-11-12 09:35:20 +0100

    adding autotag solution back

--- lua/corfitz/plugins/treesitter.lua
+++ lua/corfitz/plugins/treesitter.lua
@@ -56,4 +56,15 @@ return {
       })
     end,
   },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup({
+        -- your config
+      })
+    end,
+    lazy = true,
+    event = "VeryLazy",
+  },
 }
