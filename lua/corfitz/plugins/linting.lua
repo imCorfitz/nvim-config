4fe08d7e624d3434e8ab529fd28268366a8c8e1e
Corfitz <hi@imcorfitz.com>
2025-04-30 21:32:46 +0200

    fixed issue with eslint in monorepos

--- lua/corfitz/plugins/linting.lua
+++ lua/corfitz/plugins/linting.lua
@@ -15,10 +15,34 @@ return {
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+    local function find_nearest_node_modules_dir()
+      -- current buffer dir
+      local current_dir = vim.fn.expand("%:p:h")
+      while current_dir ~= "/" do
+        if vim.fn.isdirectory(current_dir .. "/node_modules") == 1 then
+          return current_dir
+        end
+        current_dir = vim.fn.fnamemodify(current_dir, ":h")
+      end
+      return nil
+    end
+
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
+        local ft = vim.bo.filetype
+        local js_types = { "javascript", "typescript", "javascriptreact", "typescriptreact" }
+        if not vim.tbl_contains(js_types, ft) then
+          lint.try_lint()
+          return
+        end
+        local original_cwd = vim.fn.getcwd()
+        local node_modules_dir = find_nearest_node_modules_dir()
+        if node_modules_dir then
+          vim.cmd("cd " .. node_modules_dir)
+        end
         lint.try_lint()
+        vim.cmd("cd " .. original_cwd)
       end,
     })
 
